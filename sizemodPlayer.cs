2d31615f79d1f7571bec0d156509b0fbb295f1e3
Voileexperiments <voileexperiments@gmail.com>
2016-09-14 15:10:21 +0800

    Changed the wing DrawData check to something more sensible
    Still, *cringes*

--- sizemodPlayer.cs
+++ sizemodPlayer.cs
@@ -11,10 +11,10 @@ namespace sizemod
 {
     public class sizemodPlayer : ModPlayer
     {
-        public static float playerSize = 8.0f;
+        public static float playerSize = 4.0f;
+        private static List<Texture2D> WingsExtraTexOccupied = new List<Texture2D>{Main.itemFlameTexture[1866], Main.extraTexture[38], Main.FlameTexture[8], Main.glowMaskTexture[92], Main.glowMaskTexture[181], Main.glowMaskTexture[213], Main.glowMaskTexture[183]};
         Random rnd = new Random();
-        private int framecount = 0;
-
+        
         public override void PreUpdate()
         {
             for (int k = 0; k < 255; k++)
@@ -25,12 +25,7 @@ namespace sizemod
                     Player thisPlayer = Main.player[k];
                     if (thisPlayer.active)
                     {
-                        if(framecount == 5)
-                        {
-                            //playerSize = rnd.NextFloat() * 2.0f + 0.5f; // For testing purpose only
-                            framecount = 0;
-                        }
-                        framecount++;
+                        //playerSize = rnd.NextFloat() * 2.0f + 0.5f; // For testing purpose only
 
                         // Change player hitbox size
                         thisPlayer.position = thisPlayer.Bottom;
@@ -64,8 +59,6 @@ namespace sizemod
 
                         // Change player max speed
                         thisPlayer.maxRunSpeed = player.maxRunSpeed * playerSize;
-
-                        Main.NewText(Main.playerDrawData[0].texture.Name);
                     }
                 }
             }
@@ -79,20 +72,22 @@ namespace sizemod
             for (int i = 0; i < count; i++)
             {
                 Player thisPlayer = Main.player[Main.myPlayer];
-                int p = WingsDrawLayerUsed(thisPlayer, 0f);
                 var d = Main.playerDrawData[i];
                 d.scale *= playerSize;
                 Vector2 texOffset = new Vector2(d.sourceRect.Value.Width, d.sourceRect.Value.Height) - d.origin;
                 texOffset.X -= d.sourceRect.Value.Width / 2;
-                //d.origin = new Vector2(d.sourceRect.Value.Width, d.sourceRect.Value.Height);
                 Vector2 thisPlayerPosOffset = (thisPlayer.Bottom - Main.screenPosition) - d.position;
                 Vector2 totalOffset = thisPlayerPosOffset;
-                if (i < p) totalOffset.Y = 0;
+                if (d.texture == Main.wingsTexture[thisPlayer.wings]) totalOffset.Y = 0;
+                for (int j = 0; j < WingsExtraTexOccupied.Count; j++)
+                {
+                    if (d.texture == WingsExtraTexOccupied[j]) totalOffset.Y = 0;
+                }
+
                 d.position -= totalOffset * (playerSize - 1f);
                 d.position.Y -= 2f * (playerSize - 1f);
                 Main.playerDrawData[i] = d;
             }
-            
         });
 
         public override void ModifyDrawLayers(List<PlayerLayer> layers)
@@ -140,54 +135,6 @@ namespace sizemod
             {
                 return 1.0f;
             }
-        }
-
-        private static int WingsDrawLayerUsed(Player player, float shadow)
-        {
-            int count = 0;
-
-            bool flag12 = (player.wings == 0 || player.velocity.Y == 0f) && (player.inventory[player.selectedItem].type == 1178 || player.inventory[player.selectedItem].type == 779 || player.inventory[player.selectedItem].type == 1295 || player.inventory[player.selectedItem].type == 1910 || player.turtleArmor || player.body == 106 || player.body == 170);
-            if (!flag12 && player.wings > 0)
-            {
-                if (player.wings == 22)
-                {
-                    if ((player.velocity.Y != 0f || player.grappling[0] != -1) && !player.mount.Active)
-                    {
-                        count++;
-                        if (shadow == 0f && player.grappling[0] == -1)
-                        {
-                            count += 7;
-                        }
-                    }
-                }
-                else if (player.wings == 28)
-                {
-                    if ((player.velocity.Y != 0f || player.grappling[0] != -1) && !player.mount.Active)
-                    {
-                        count += 2;
-                    }
-                }
-                else if (player.wings == 34)
-                {
-                    if ((player.velocity.Y != 0f || player.grappling[0] != -1) && !player.mount.Active)
-                    {
-                        count++;
-                    }
-                }
-                else
-                {
-                    count++;
-                    if (player.wings == 23 || player.wings == 27 || player.wings == 29 || player.wings == 30 || player.wings == 32)
-                    {
-                        count++;
-                    }
-                    else if (player.wings == 31 || player.wings == 36)
-                    {
-                        count += 5;
-                    }
-                }
-            }
-            return count;
         }
     }
 }
